50b946cdc71e1289729a625d0da4c44ca6884d3a
ethan2004g <ethan2004g@gmail.com>
2025-03-21 18:42:58 -0500

    Added function signature and initialized distance and parent vectors

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -11,3 +11,15 @@ typedef pair<int, long long> Edge;
 
 // Store (distance, node) in the priority queue
 typedef pair<long long, int> DistNode;
+
+pair<vector<int>, long long> dijkstra_path(const vector<vector<Edge>>& graph, int start, int end) {
+    // How many nodes we have
+    int n = (int)graph.size();
+
+    // dist[node] = the shortest distance we know from 'start' to this node
+    vector<long long> dist(n, LLONG_MAX);
+    dist[start] = 0;
+
+    // parent[node] = the node we came from on the best path to 'node'
+    vector<int> parent(n, -1);
+}
