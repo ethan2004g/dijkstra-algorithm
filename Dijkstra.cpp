4dcd38a3819efdf421842097cb4c1ffb4f365e00
ethan2004g <ethan2004g@gmail.com>
2025-03-21 19:05:28 -0500

    Added priority queue for processing nodes in Dijkstra's algorithm

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 // Store edges as (neighbor, weight)
 typedef pair<int, long long> Edge;
-
 // Store (distance, node) in the priority queue
 typedef pair<long long, int> DistNode;
 
@@ -22,4 +21,14 @@ pair<vector<int>, long long> dijkstra_path(const vector<vector<Edge>>& graph, in
 
     // parent[node] = the node we came from on the best path to 'node'
     vector<int> parent(n, -1);
+
+    // Using a min-heap: (current_distance, node)
+    auto compare = [](const DistNode& a, const DistNode& b) {
+        return a.first > b.first; // We want the smaller distance on top
+    };
+    priority_queue<DistNode, vector<DistNode>, decltype(compare)> pq(compare);
+
+    // Push (0, start) onto our queue to begin
+    pq.push({0, start});
+    
 }
